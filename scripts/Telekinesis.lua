52b9ab480c76d38a6526f567b599a17ce2c45329
randomguye <102664681+randomguye@users.noreply.github.com>
2025-05-03 15:53:37 +0800

    Update Telekinesis.lua

--- scripts/Telekinesis.lua
+++ scripts/Telekinesis.lua
@@ -465,8 +465,8 @@ local createtool = function()
 			local direction = Vector3.new(0,-1,0);
 			mousedown = false;
 			w()
-			orgobj.Velocity = direction * 75000
-			orgobj.RotVelocity = direction * 75000
+			orgobj.Velocity = direction * 50000
+			orgobj.RotVelocity = direction * 50000
 			CollideWav:Play()
 		end
 	end;
