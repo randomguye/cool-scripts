eb57cc7a471923f667a7a25c9bf638d6047477ee
randomguye <102664681+randomguye@users.noreply.github.com>
2025-05-04 13:51:41 +0800

    Update AdonisBypass.lua

--- scripts/AdonisBypass.lua
+++ scripts/AdonisBypass.lua
@@ -1,1 +1,51 @@
+local getinfo = getinfo or debug.getinfo
+local Hooked = {}
 
+local Detected, Kill
+
+setthreadidentity(2)
+
+for i, v in getgc(true) do
+    if typeof(v) == "table" then
+        local DetectFunc = rawget(v, "Detected")
+        local KillFunc = rawget(v, "Kill")
+    
+        if typeof(DetectFunc) == "function" and not Detected then
+            Detected = DetectFunc
+            
+            local Old; Old = hookfunction(Detected, function(Action, Info, NoCrash)
+                if Action ~= "_" then
+                    warn(`Adonis AntiCheat flagged\nMethod: {Action}\nInfo: {Info}`)
+                end
+                
+                return true
+            end)
+
+            table.insert(Hooked, Detected)
+        end
+
+        if rawget(v, "Variables") and rawget(v, "Process") and typeof(KillFunc) == "function" and not Kill then
+            Kill = KillFunc
+            local Old; Old = hookfunction(Kill, function(Info)
+                warn(`Adonis AntiCheat tried to kill (fallback): {Info}`)
+            end)
+
+            table.insert(Hooked, Kill)
+        end
+    end
+end
+
+local Old; Old = hookfunction(getrenv().debug.info, newcclosure(function(...)
+    local LevelOrFunc, Info = ...
+
+    if Detected and LevelOrFunc == Detected then
+        if true then
+            warn(`LuaDev | adonis bypassed`)
+        end
+
+        return coroutine.yield(coroutine.running())
+    end
+    
+    return Old(...)
+end))
+setthreadidentity(8)
